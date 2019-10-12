51b5fb64fca4e09a799e9fda15d64dbbb6c7ba02
Thomas <tmathew@sfu.ca>
2019-10-11 22:36:03 -0700

    Testing database

--- index.js
+++ index.js
@@ -27,5 +27,17 @@ express()
       res.send("Error " + err);
     }
   })
+  .get('/users', (req,res) => {
+    var getUsersQuery = `SELECT * FROM userstab`;
+    console.log(getUsersQuery);
+    pool.query(getUsersQuery, (error, result) => {
+      if (error)
+        res.end(error);
+      var results = {'rows': result.rows };
+      console.log(results);
+      res.render('pages/users', results)
+    });
+  })
+  
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
