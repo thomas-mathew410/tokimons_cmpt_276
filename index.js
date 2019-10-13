8ad3a76e9887e2cc053893873c7b3b83ada88421
Thomas <tmathew@sfu.ca>
2019-10-12 18:15:56 -0700

    Form

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const bodyParser = require('body-parser')
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -10,10 +11,13 @@ const pool = new Pool({
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(express.urlencoded({extended : false}))
+  .use(bodyParser())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/landingpage', (req, res) => res.render('pages/landing'))
+  .get('/add', (req, res) => res.render('pages/addnew'))
   // .get('/landingpage', (req, res) => res.sendfile(path.join(__dirname+'/views/pages/landing.ejs')))
   .get('/db', async (req, res) => {
     try {
@@ -27,6 +31,7 @@ express()
       res.send("Error " + err);
     }
   })
+
   .get('/users', (req,res) => {
     var getUsersQuery = `SELECT * FROM userstab`;
     console.log(getUsersQuery);
@@ -37,7 +42,21 @@ express()
       console.log(results);
       res.render('pages/users', results)
     });
-  })
+  })  
+  
+  .post('/add', (req,res) => {
+
+    var updateUsersQuery = `INSERT INTO userstab (username) VALUES (${req.body["word"]})`;
+    console.log(updateUsersQuery);
+    pool.query(updateUsersQuery, (error, result) => {
+      if (error)
+        res.end(error);
+      console.log('Hello');
+      var results = {'rows': result.rows };
+      console.log(results);
+      res.send('POST request to the homepage')
+    });
+  }) 
   
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
