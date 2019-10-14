081ee970339540ed256ce2fd343fcadee83f9348
Thomas <tmathew@sfu.ca>
2019-10-14 13:10:27 -0700

    check

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ express()
         res.end(error);
       var results = {'rows': result.rows };
       
-      res.json(result.rows.id);
+      res.json(result.rows);
       // console.log(results);
       // res.redirect(301, `/users`);
     });
